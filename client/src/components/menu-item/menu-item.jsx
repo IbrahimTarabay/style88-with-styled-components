4f5c854d0d8c31a43207c1587994f0cf8764b255
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-03-25 02:08:37 +0200

    make menu-item styled-components

--- client/src/components/menu-item/menu-item.jsx
+++ client/src/components/menu-item/menu-item.jsx
@@ -9,17 +9,16 @@ import {
   ContentSubtitle
 } from './menu-item.styles';
 
-import './menu-item.scss';
-
-const MenuItem = ({title,imageUrl,size,history,match,linkUrl}) =>{
-  return (<div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }}></div>
-    <div className='content'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <span className='subtitle'>SHOP NOW</span>
-    </div>
-  </div>);
-};
+const MenuItem = ({title,imageUrl,size,history,match,linkUrl}) =>
+  (<MenuItemContainer
+   size={size}
+   onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <BackgroundImageContainer className='background-image' imageUrl={imageUrl}></BackgroundImageContainer>
+    <ContentContainer>
+      <ContentTitle>{title.toUpperCase()}</ContentTitle>
+      <ContentSubtitle>SHOP NOW</ContentSubtitle>
+    </ContentContainer>
+  </MenuItemContainer>);
 
 export default withRouter(MenuItem);
 
