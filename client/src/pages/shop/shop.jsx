602272661e2f331b1fceea458bc2d3e6642232cb
IbrahimTarabay <ibrahimesalah69@gmail.com>
2020-03-27 21:58:28 +0200

    add lazy and suspense to shop.jsx

--- client/src/pages/shop/shop.jsx
+++ client/src/pages/shop/shop.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {lazy,Suspense} from 'react';
 import {Route} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import Spinner from '../../components/spinner/spinner';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
 
 class ShopPage extends React.Component{
   componentDidMount(){
@@ -19,8 +21,10 @@ class ShopPage extends React.Component{
   <Route path='/shop' component={ShopPage} /> which in App.js*/
   return(
   <div className='shop-page'>
+   <Suspense> 
     <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
     <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}/>
+   </Suspense>
   </div>
   );
  }
