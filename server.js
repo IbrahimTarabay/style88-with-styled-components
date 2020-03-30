c593ed474ac8d393347a20218bea02ecbc9afdb2
Ibrahim_Tarabay <32605959+IbrahimTarabay@users.noreply.github.com>
2020-03-30 15:09:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,10 @@ app.listen(port,error => {
   console.log('Server running on port '+port);
 });
 
+app.get('/service-worker.js', (req,res) => {
+  res.sendFile(path.resolve(__dirname,'..','build','service-worker.js'));
+});/*.. mean to go from client to build folder to find service-worker.js*/
+
 app.post('/payment',(req,res) => {
   const body = {
     source: req.body.token.id,
