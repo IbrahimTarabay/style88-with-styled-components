7da390c7f7aadb8c1ed211d242e308163de3a50f
Ibrahim_Tarabay <32605959+IbrahimTarabay@users.noreply.github.com>
2020-04-07 12:11:39 +0200

    Update shop.jsx

--- client/src/pages/shop/shop.jsx
+++ client/src/pages/shop/shop.jsx
@@ -1,36 +1,46 @@
-import React from 'react';
-import {lazy,Suspense} from 'react';
-import {Route} from 'react-router-dom';
-import {connect} from 'react-redux';
+import React, { useEffect, lazy, Suspense } from 'react';
+import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
-const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
+import Spinner from '../../components/spinner/spinner';
 
-class ShopPage extends React.Component{
-  componentDidMount(){
-   const {fetchCollectionsStart} = this.props;
-   fetchCollectionsStart();
-  }
+const CollectionsOverviewContainer = lazy(() =>
+  import('../../components/collections-overview/collections-overview.container')
+);
 
- render(){ 
-   const {match} = this.props;
+const CollectionPageContainer = lazy(() =>
+  import('../collection/collection.container')
+);
+
+export const ShopPage = ({ fetchCollectionsStart, match }) => {
    /*we have access to match object because 
   <Route path='/shop' component={ShopPage} /> which in App.js*/
-  return(
-  <div className='shop-page'>
-   <Suspense> 
-    <Route exact path={`${match.path}`} component={CollectionsOverviewContainer} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer}/>
-   </Suspense>
-  </div>
-  );
- }
-}
+  useEffect(() => {
+    fetchCollectionsStart();
+  }, [fetchCollectionsStart]);
 
-const mapDispatchToProps = dispatch =>({
+  return (
+      <Suspense fallback={<Spinner />}>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </Suspense>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
-export default connect(null,mapDispatchToProps)(ShopPage);
+export default connect(
+  null,
+  mapDispatchToProps
+)(ShopPage);
